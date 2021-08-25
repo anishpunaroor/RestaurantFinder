be7216d88df0ea6ed39e7c99e1611ba797c8154e
anishpunaroor <anish.punaroor@gmail.com>
2021-08-25 13:10:31 -0700

    fix leak

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,23 @@ import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 8000
 
+// Connect to database 
 MongoClient.connect(
-    process.env.
-)
+    process.env.RESTFINDER_DB_URI, 
+    {
+        poolSize: 50, 
+        wtimeout: 2500, 
+        useNewUrlParse: true }
+    )
+    // Check and record any errors
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
