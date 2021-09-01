4d91fdfbb07f25ac172f19999ce0f698b696d237
Anish Punaroor <anish.punaroor@gmail.com>
2021-09-01 14:16:23 -0700

    Delete response

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -45,4 +45,17 @@ export default class ReviewsDAO {
             return { error: e }
         }
     }
+
+    static async deleteReview(reviewId, userId) {
+        try {
+            const deleteResponse = await.reviews.deleteResponse({
+                _id: ObjectId(reviewId), 
+                user_id: userId, 
+            })
+            return deleteResponse
+        } catch (e) {
+            console.error(`Unable to delete review: ${e}`)
+            return { error: e }
+        }
+    }
 }
